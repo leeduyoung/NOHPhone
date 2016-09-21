b4cba4d802dcd1ce77de45189505180e9b72e04c
이두영 <leeduyoung20@naver.com>
2016-09-21 22:30:25 +0900

    하단 탭 구성

---
+++ www/js/factory/urls.factory.js
@@ -0,0 +1,18 @@
+angular.module('starter')
+    .factory('UrlsFactory', function(){
+
+        /* device 용 */
+        // var host = 'http://52.78.5.104:8081';
+        // var host = 'http://192.168.0.8:8081';
+
+        /* web 용 */
+        var host = '';
+
+        var urls = {
+            
+        }
+
+        return {
+            urls
+        }
+    });
