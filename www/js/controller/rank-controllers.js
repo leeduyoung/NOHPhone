b3c0da12eca48f5df9ec48e71fcdf945fb765fa1
이두영 <leeduyoung20@naver.com>
2016-09-22 22:31:11 +0900

    period(day,week,total) button bar 추가

--- www/js/controller/rank-controllers.js
+++ www/js/controller/rank-controllers.js
@@ -2,4 +2,20 @@ angular.module('starter.controllers', [])
 
     .controller('RankCtrl', function ($scope) {
 
+        $scope.getRank = getRank;
+
+        $scope.periodList = [
+            { text: "하루", value: "one"},
+            { text: "일주일", value: "week"},
+            { text: "전체", value: "total"}
+        ];
+
+        $scope.data = {
+            period: "하루"
+        };
+
+        function getRank(period) {
+            console.log("getRank");
+            console.log(period);
+        }
     });
